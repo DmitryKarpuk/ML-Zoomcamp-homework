43de41dc3650d848ecc8b5e7df43f261437ff7e6
DmitryKarpuk <dimakarpuk22@gmail.com>
2022-10-22 11:44:05 +0300

    7hw done

--- HW 07-bento-production/service.py
+++ HW 07-bento-production/service.py
@@ -1,16 +1,47 @@
+import numpy as np
+from pydantic import BaseModel
 import bentoml
-
 from bentoml.io import JSON
 
-model_ref = bentoml.xgboost.get("credit_model:latest")
-dv = model_ref.custom_objects['DictVectorizer']
+class UserProfile():
+    seniority : int
+    home : str
+    time: int
+    age: int
+    marital: str
+    records: str
+    job: str
+    expenses: int
+    income: float
+    assets: float
+    debt: float
+    amount: int
+    price : int
+
+model_ref = bentoml.xgboost.get("credit_risk_model:latest")
+dv = model_ref.custom_objects['dictVectorizer']
 
 model_runner = model_ref.to_runner()
 
-svc = bentoml.Service('credit_model', runners=[model_runner])
+svc = bentoml.Service("credit_risk_classifier", runners=[model_runner])
 
-@svc.api(input=JSON(), output=JSON())
-def classify(application_data):
+
+@svc.api(input=JSON(pydantic_model=UserProfile), output=JSON())
+async def classify(application_data):
     vector = dv.transform(application_data)
-    prediction = model_runner.predict.run(vector)
-    return {"status" : "Approve"}
+    prediction = await model_runner.predict.async_run(vector)
+    print(prediction)
+    result = prediction[0]
+
+    if result > 0.5:
+        return {
+            "status": "DECLINED"
+        }
+    elif result > 0.25:
+        return {
+            "status": "MAYBE"
+        }
+    else:
+        return {
+            "status": "APPROVED"
+        }
